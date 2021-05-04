245cce877ac6fc9b0f6c0b6b06e30c41429e86ab
Cameron King <cking@ckxng.com>
2021-05-03 22:46:25 -0500

    adds doc comments

--- TeamScheduleViewer/Service/IPlanLookupService.cs
+++ TeamScheduleViewer/Service/IPlanLookupService.cs
@@ -5,9 +5,11 @@ namespace TeamScheduleViewer.Service
     public interface IPlanLookupService
     {
         /// <summary>
-        ///
+        /// Connects to the PCO API to find all plans within the gloablly configured service type.
+        /// For each plan, the a secondary API call is made to look up the schedule team members 
+        /// for that date.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>an array of plans, populated with team members</returns>
         IPlan[] PlansInServiceType();
     }
 }
